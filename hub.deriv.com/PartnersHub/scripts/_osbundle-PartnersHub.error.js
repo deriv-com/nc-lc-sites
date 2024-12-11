bd456538dbc0ff767832224614edf11f5584f23a
security-deriv <x-securitygithub@regentmarkets.com>
2024-12-11 05:14:27 +0000

    Update Wed Dec 11 05:14:27 UTC 2024

--- hub.deriv.com/PartnersHub/scripts/_osbundle-PartnersHub.error.js
+++ hub.deriv.com/PartnersHub/scripts/_osbundle-PartnersHub.error.js
@@ -1,7 +1,7 @@
 try {
     Promise.all([import("./_oschunk-7KS6UKKW.js")]).then(function(r) {
         var o = r[0];
-        Promise.all([import("./_oschunk-7GWZ6HW5.js"), import("./_oschunk-JVSAMRW4.js")]).then(function(i) {
+        Promise.all([import("./_oschunk-7GWZ6HW5.js"), import("./_oschunk-QM4GZECO.js")]).then(function(i) {
             var e = i[0],
                 n = i[1].default,
                 t = e;
