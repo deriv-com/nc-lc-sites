4d6a3cdd53a664997329346f526b607f78e0ecf3
Shantanu <125633368+shantanug-deriv@users.noreply.github.com>
2025-11-07 14:42:35 +0800

    Delete hub.deriv.com directory
    removing stale code

--- hub.deriv.com/tradershub/scripts/_oschunk-CVX56YKZ.js
+++
@@ -1,9 +0,0 @@
-import {
-    a
-} from "./_oschunk-LXQ572EY.js";
-import "./_oschunk-2JKANR6M.js";
-import "./_oschunk-DVBKI63I.js";
-export {
-    a as
-    default
-};
--- hub.deriv.com/tradershub/scripts/_oschunk-N4M2554N.js
+++
@@ -1,9 +0,0 @@
-import {
-    a
-} from "./_oschunk-2M7PXTSQ.js";
-import "./_oschunk-2JKANR6M.js";
-import "./_oschunk-DVBKI63I.js";
-export {
-    a as
-    default
-};
